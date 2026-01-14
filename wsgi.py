cb5f52c04353482609f760a1cc5abe1b0b83612a
Chris Moreton <chris@netsensia.com>
2026-01-14 17:41:10 +0000

    Add WSGI entry point and Procfile for Railway deployment

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+"""WSGI entry point for the Flask application."""
+
+import os
+from web.app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
